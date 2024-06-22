8f033e0a21bd6e4760490014750a0ae7a0b77222
Akshai P R <akshai4610@gmail.com>
2024-06-22 17:18:39 +0530

    Initial commit,SideBar component added...

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SideBar from "./Components/SideBar";
+
+function App() {
+  return (
+    <main>
+      <SideBar/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,11 @@
+export default function SideBar(){
+    return(
+        <aside>
+            <h1>Your Project</h1>
+            <div>
+                <button>+ Add Project</button>
+            </div>
+            <ul></ul>
+        </aside>
+    )
+}
