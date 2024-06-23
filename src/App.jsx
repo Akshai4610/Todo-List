147e6b88f021a05c2f8ab9308cfd3e918cbb5f3d
Akshai P R <akshai4610@gmail.com>
2024-06-23 22:26:23 +0530

    Updation 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import SideBar from "./Components/SideBar";
 import NewProject from "./Components/NewProject";
+import Display from "./Components/Display";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setProjects] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleChange() {
+    setProjects((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: null,
+      };
+    });
+  }
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <Display btnClicked={handleChange} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SideBar/>
-      <NewProject/>
+      <SideBar btnClicked={handleChange} />
+      {content}
     </main>
   );
 }
